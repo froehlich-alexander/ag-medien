0c5c7852c2b03e5fe85c0032478b896401ab72aa
Vertretungsplan App <90785728+vertretungsplan1@users.noreply.github.com>
2022-03-17 16:07:33 +0100

    Rein- und Rauslaufen funktioniert jetzt, Behebung von Bugs

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
+var finished_last=true;
 $(document).ready(function(){
     if(window.location.hash!=="")
         $("#tour_pg_"+window.location.hash.slice(1)).addClass("show");
     else
         $(".page:eq(0)").addClass("show");
-    for(var a=0;a<$(".bg").length;++a){
-        var imgRatio=$(".bg")[a].naturalWidth/$(".bg")[a].naturalHeight;
+    $("img.bg").one("load", function(){
+        $(this).removeClass("fill-width");
+        $(this).removeClass("fill-height");
+        var imgRatio=this.naturalWidth/this.naturalHeight;
         var screenRatio=window.innerWidth/window.innerHeight;
         if(imgRatio>screenRatio)
-            $($(".bg")[a]).addClass("fill-width");
+            $(this).addClass("fill-width");
         else
-            $($(".bg")[a]).addClass("fill-height");
-    }
+            $(this).addClass("fill-height");
+        adjust_clickables();
+    }).each(function() {
+        if(this.complete) {
+            $(this).trigger('load');
+        }
+      });
     for(let a=0;a<$(".clickable").length;++a){
         $($(".clickable")[a]).css("left",$($(".clickable")[a]).attr("data-x")+"%");
         $($(".clickable")[a]).css("top",$($(".clickable")[a]).attr("data-y")+"%");
-        $($(".clickable")[a]).find(".icon").click(function(){goTo($($(".clickable")[a]).attr("data-goto"))});
+        $($(".clickable")[a]).find(".icon").click(function(){goTo($($(".clickable")[a]).attr("data-goto"),$($(".clickable")[a]).attr("data-backward")!==undefined&&$($(".clickable")[a]).attr("data-backward").length>0)});
     }
-    adjust_clickables();
+
 });
-$(".bg").on('load',function(){
-    var imgRatio=this.naturalWidth/this.naturalHeight;
-    var screenRatio=window.innerWidth/window.innerHeight;
-    if(imgRatio>screenRatio)
-        $(this).addClass("fill-width");
-    else
-        $(this).addClass("fill-height");
-});
+
 window.onresize=function(){
     $(".bg").removeClass("fill-width");
     $(".bg").removeClass("fill-height");
@@ -46,16 +47,34 @@ window.onpopstate=function(){
     else
         $(".page:eq(0)").addClass("show");
 }
-function goTo(pg){
-    $(".page.show").addClass("walk_out_forward");
-    $(".page.show").removeClass("show");
-    $("#tour_pg_"+pg).addClass("show");
-    setTimeout(function(){
-        $(".page.walk_out_forward").removeClass("show");
-        $(".page.walk_out_forward").removeClass("walk_out_forward");
-    },500);
-    window.location.hash=pg;
-    adjust_clickables();
+function goTo(pg,backward){
+    if(finished_last){
+        finished_last=false;
+        if(!backward){
+            $(".page.show").addClass("walk_in_out");
+            $("#tour_pg_"+pg).addClass("show");
+            $("#tour_pg_"+pg).addClass("walk_in_in");
+            setTimeout(function(){
+                $(".page.walk_in_in").removeClass("walk_in_in");
+                $(".page.walk_in_out").removeClass("show");
+                $(".page.walk_in_out").removeClass("walk_in_out");
+                finished_last=true;
+            },500);
+        }
+        else{
+            $(".page.show").addClass("walk_out_out");
+            $("#tour_pg_"+pg).addClass("show");
+            $("#tour_pg_"+pg).addClass("walk_out_in");
+            setTimeout(function(){
+                $(".page.walk_out_in").removeClass("walk_out_in");
+                $(".page.walk_out_out").removeClass("show");
+                $(".page.walk_out_out").removeClass("walk_out_out");
+                finished_last=true;
+            },500);
+        }
+        window.location.hash=pg;
+        adjust_clickables();
+    }
 }
 function adjust_clickables(){
     $(".clickable").removeClass("right");
